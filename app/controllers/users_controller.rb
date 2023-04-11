ac3acf597479eb206064d70c3c0c512c827cfc29
Evan Burton <evanburton77@gmail.com>
2023-04-11 19:47:13 +0000

    Completed authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
+  def new_registration_form
+    render({ :template => "users/signup_form.html.erb" })
+  end
+
+  def authenticate
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    user = User.where({ :username => un }).first
+
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "no user matches this username" })
+    else
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+        redirect_to("/", { :notice => "Welcome back, " + user.username })
+      else
+        redirect_to("/user_sign_in", { :alert => "Incorrect password" })
+      end
+    end
+  end
+
+  def new_session_form
+    render({ :template => "users/signin_form.html.erb" })
+  end
+
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -12,25 +38,38 @@ class UsersController < ApplicationController
     render({ :template => "users/show.html.erb" })
   end
 
+  def toast_cookies
+    reset_session
+
+    redirect_to("/", { :notice => "See you later!" })
+  end
+
   def create
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id)
+
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!" })
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
+    end
   end
 
   def update
     the_id = params.fetch("the_user_id")
     user = User.where({ :id => the_id }).at(0)
 
-
     user.username = params.fetch("input_username")
 
     user.save
-    
+
     redirect_to("/users/#{user.username}")
   end
 
@@ -42,5 +81,4 @@ class UsersController < ApplicationController
 
     redirect_to("/users")
   end
-
 end
